194feba36f9dfa522b05a50bc42883ce3ac53146
Sushant Chaudhary <sushant@Sushants-MacBook-Air.local>
2018-02-21 17:44:00 -0800

    Added a simple reddit scraper, just replace link to change reddit thread

--- RedditCommentScraper.py
+++ RedditCommentScraper.py
@@ -1,37 +1,22 @@
-import requests
-import requests.auth
+'''
+Program to generate top comments from any thread on Reddit and put it into a csv file with column name Top Comments.
+'''
+
 import praw
+from praw.models import MoreComments
+import csv
 
-'''client_auth = requests.auth.HTTPBasicAuth('p-jcoLKBynTLew', 'gko_LXELoV07ZBNUXrvWZfzE3aI')
-post_data = {"grant_type": "password", "username": "reddit_bot", "password": "snoo"}
-headers = {"User-Agent": "ChangeMeClient/0.1 by YourUsername"}
-response = requests.post("https://www.reddit.com/api/v1/access_token", auth=client_auth, data=post_data, headers=headers)
-response.json()'''
+reddit = praw.Reddit(user_agent='CommentScraper (by /u/ConnectBad)',
+                     client_id='dHUYT7-TgK2aKA', client_secret="Sg2ZreLSzOYqURbJK4QA5Ysh7tM",
+                     username='ConnectBad', password='passwordfor180')
 
-username = 'ConnectBad'
-userAgent = "CommentScraper/0.1 by " + username
-clientId = 'dHUYT7-TgK2aKA'
-clientSecret = "Sg2ZreLSzOYqURbJK4QA5Ysh7tM"
-password = "passwordfor180"
-r = praw.Reddit(user_agent=userAgent, client_id=clientId, client_secret=clientSecret)
+submission = reddit.submission(url='https://www.reddit.com/r/funny/comments/3g1jfi/buttons/')
 
-def getSubComments(comment, allComments, verbose=True):
-  allComments.append(comment)
-  if not hasattr(comment, "replies"):
-    replies = comment.comments()
-    if verbose: print("fetching (" + str(len(allComments)) + " comments fetched total)")
-  else:
-    replies = comment.replies
-  for child in replies:
-    getSubComments(child, allComments, verbose=verbose)
+myFile = open('topcomments.csv', 'w') 
+writer = csv.writer(myFile, dialect='excel')
+writer.writerow(['Top Comments'])
 
-
-def getAll(r, submissionId, verbose=True):
-  submission = r.submission(submissionId)
-  comments = submission.comments
-  commentsList = []
-  for comment in comments:
-    getSubComments(comment, commentsList, verbose=verbose)
-  return commentsList
-
-print(reddit.user.me())
+for top_level_comment in submission.comments:
+    if isinstance(top_level_comment, MoreComments):
+        continue
+    writer.writerow([top_level_comment.body])
