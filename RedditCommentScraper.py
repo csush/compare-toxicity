8d11bebec61ffa019ae4b7593ea240d2ef3527ce
Sushant Chaudhary <sushant@Sushants-MacBook-Air.local>
2018-02-21 18:14:47 -0800

    changed scraper to generate all comments

--- RedditCommentScraper.py
+++ RedditCommentScraper.py
@@ -10,13 +10,13 @@ reddit = praw.Reddit(user_agent='CommentScraper (by /u/ConnectBad)',
                      client_id='dHUYT7-TgK2aKA', client_secret="Sg2ZreLSzOYqURbJK4QA5Ysh7tM",
                      username='ConnectBad', password='passwordfor180')
 
-submission = reddit.submission(url='https://www.reddit.com/r/funny/comments/3g1jfi/buttons/')
+submission = reddit.submission(url='https://www.reddit.com/r/worldnews/comments/7za8v1/trump_endorses_guns_for_teachers_to_stop_shootings/')
 
 myFile = open('topcomments.csv', 'w') 
 writer = csv.writer(myFile, dialect='excel')
 writer.writerow(['Top Comments'])
 
-for top_level_comment in submission.comments:
-    if isinstance(top_level_comment, MoreComments):
-        continue
-    writer.writerow([top_level_comment.body])
+submission.comments.replace_more(limit=None)
+for comment in submission.comments.list():
+    writer.writerow([comment.body.encode('utf-8')])
+    
