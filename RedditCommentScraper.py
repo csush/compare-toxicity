f145d5e1b927fde5efbe3b00bebd66f1f6acf12d
Sushant Chaudhary <sushant@Sushants-MacBook-Air.local>
2018-02-26 18:42:15 -0800

    extracts 1000 comments from each post on hot page's top 10

--- RedditCommentScraper.py
+++ RedditCommentScraper.py
@@ -3,19 +3,21 @@ Program to generate top comments from any thread on Reddit and put it into a csv
 '''
 
 import praw
-from praw.models import MoreComments
 import csv
 
 reddit = praw.Reddit(user_agent='CommentScraper (by /u/ConnectBad)',
                      client_id='dHUYT7-TgK2aKA', client_secret="Sg2ZreLSzOYqURbJK4QA5Ysh7tM",
                      username='ConnectBad', password='passwordfor180')
 
-submission = reddit.submission(url='https://www.reddit.com/r/worldnews/comments/7za8v1/trump_endorses_guns_for_teachers_to_stop_shootings/')
+subreddit = reddit.subreddit('soccer')
+
+#submission = reddit.submission(url='https://www.reddit.com/r/worldnews/comments/7za8v1/trump_endorses_guns_for_teachers_to_stop_shootings/')
 
 myFile = open('all_reddit_comments.csv', 'w') 
 writer = csv.writer(myFile, dialect='excel')
-writer.writerow(['Top Comments'])
+writer.writerow(['First 1000 Comments for each post on Hot Page\'s top 10'])
 
-submission.comments.replace_more(limit=None)
-for comment in submission.comments.list():
-    writer.writerow([comment.body.encode('utf-8')])
+for submission in subreddit.hot(limit=10):
+	submission.comments.replace_more(limit=1000)
+	for comment in submission.comments.list():
+		writer.writerow([comment.body.encode('utf-8')])
