adc8536de81c0903fb563a393b37d8e939820941
Sushant Chaudhary <sushant@Sushants-MacBook-Air.local>
2018-03-07 16:24:02 -0800

    Extracts first 1000 comments from top 40 posts of past month

--- RedditCommentScraper.py
+++ RedditCommentScraper.py
@@ -9,15 +9,15 @@ reddit = praw.Reddit(user_agent='CommentScraper (by /u/ConnectBad)',
                      client_id='dHUYT7-TgK2aKA', client_secret="Sg2ZreLSzOYqURbJK4QA5Ysh7tM",
                      username='ConnectBad', password='passwordfor180')
 
-subreddit = reddit.subreddit('soccer')
+subreddit = reddit.subreddit('hockey')
 
 #submission = reddit.submission(url='https://www.reddit.com/r/worldnews/comments/7za8v1/trump_endorses_guns_for_teachers_to_stop_shootings/')
 
-myFile = open('all_reddit_comments.csv', 'w') 
+myFile = open('hockey_reddit_comments.csv', 'w') 
 writer = csv.writer(myFile, dialect='excel')
-writer.writerow(['First 1000 Comments for each post on Hot Page\'s top 10'])
+writer.writerow(['First 1000 Comments for each post on Hot Page\'s top 50'])
 
-for submission in subreddit.hot(limit=10):
+for submission in subreddit.top(limit=40):
 	submission.comments.replace_more(limit=1000)
 	for comment in submission.comments.list():
 		writer.writerow([comment.body.encode('utf-8')])
